8db8ea2acc01fee1651c3f52dc552c471018db40
Serhiy <eserpav@gmail.com>
2017-08-26 21:59:32 +0300

    Autoformat of BoatController.h

--- BoatController.h
+++ BoatController.h
@@ -4,27 +4,29 @@
 
 class Boat {
 
+  private:
     byte   Speed = 0;       // Allowed range is 0..127
     int8_t Rotation = 0;    // counterclockwise < 0 < clockwise
-  public: bool   Reverse = false;
 
-  public: Motor motorLeft;
-  public: Motor motorRight;
-  Gyro  boatGyro;
+  public:
+    bool  Reverse = false;
+    Motor motorLeft;
+    Motor motorRight;
+    Gyro  boatGyro;
 
-  public: void setSpeed(int newSpeed) {
+    void setSpeed(int newSpeed) {
       if ( 0 <= newSpeed and newSpeed <= 127 ) {
         Speed = newSpeed;
       };
     };
 
-  public: void setRotation(int newRotation) {
+    void setRotation(int newRotation) {
       if ( -5 <= newRotation and newRotation <= 5 ) {
         Rotation = newRotation;
       };
     };
 
-  public: void updateMotors() {
+    void updateMotors() {
       int motLeft  = Speed + (Rotation << 3);
       int motRight = Speed - (Rotation << 3);
 
