da0c89291a3f67e0dd8b24358809fece979de76d
Sergey <eserpav@gmail.com>
2017-09-24 16:40:26 +0300

    Added Motor.pinPWM

--- Motor.h
+++ Motor.h
@@ -1,34 +1,48 @@
 #define INACTIVE_PWM 8 // Level when motor does not rotate, value in range 0..127
 
 class Motor {
+  private:
     int8_t Speed = 0; // -127..+127
     byte pinPlus;
     byte pinMinus;
+    byte pinPWM;
 
-  public: void setPinPlus(byte boardPin) {
+  public:
+    void setPinPlus(byte boardPin) {
       pinMode(boardPin, OUTPUT);
       pinPlus = boardPin;
     }
 
-  public: void setPinMinus(byte boardPin) {
+    void setPinMinus(byte boardPin) {
       pinMode(boardPin, OUTPUT);
       pinMinus = boardPin;
     }
 
-  public: void setSpeed(int8_t newSpeed) {
+    void setPinPWM(byte boardPin) {
+      pinMode(boardPin, OUTPUT);
+      pinPWM = boardPin;
+    }
+
+    void setSpeed(int8_t newSpeed) {
       if ( abs(newSpeed) <= INACTIVE_PWM ) {
         Speed = 0;
       } else {
         Speed = newSpeed;
       };
-      int16_t pwm = abs(newSpeed) << 3;
+      int16_t pwm = abs(Speed) << 3;
       if ( Speed >= 0 ) {
-        analogWrite(pinMinus, 0);
-        analogWrite(pinPlus,  pwm);
+        digitalWrite(pinMinus, 0);
+        digitalWrite(pinPlus,  1);
+        analogWrite(pinPWM,  pwm);
       } else {
-        analogWrite(pinPlus,  0);
-        analogWrite(pinMinus, pwm);
+        digitalWrite(pinMinus, 1);
+        digitalWrite(pinPlus,  0);
+        analogWrite(pinPWM, pwm);
       };
+    };
+
+    int8_t getSpeed() {
+        return Speed;
     };
 };
 
